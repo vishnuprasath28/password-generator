e74926dae649329a01d3cc39d1add32b9294dbe2
Vishnu Prasath.T.A <119924536+vishnuprasath28@users.noreply.github.com>
2022-12-06 13:57:49 +0530

    Update pswrd.java

--- pswrd.java
+++ pswrd.java
@@ -46,9 +46,3 @@ public class NewClass
         return password;
     }
 }
-git init
-git add-A
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/vishnuprasath28/password-generator.git
-git push -u origin main
